260e3d73ccbcb48047938156907fa487c56f2adc
ereio <git@ere.io>
2021-01-24 12:20:26 -0500

    keeping this handy

---
+++ .tools/git-rewrite-author.sh
@@ -0,0 +1,16 @@
+git filter-branch -f --env-filter '
+WRONG_EMAIL="9c7e2cbf20@pm.me"
+NEW_NAME="ereio"
+NEW_EMAIL="git@ere.io"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
